8e1263ff645cbd14e9d85e4ca861d4895859a942
hangwl <hangwei54e@hotmail.com>
2023-08-15 23:40:28 +0800

    Exercise 1.4

--- part1/src/App.js
+++ part1/src/App.js
@@ -31,7 +31,7 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development';
+  const course = 'Half Stack application development'
   const parts = [
     { name: 'Fundamentals of React', exercises: 10 },
     { name: 'Using props to pass data', exercises: 7 },
