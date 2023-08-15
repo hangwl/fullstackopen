b986ac9d50ba663a826d15219ecb0dba01724c6e
hangwl <hangwei54e@hotmail.com>
2023-08-15 23:38:19 +0800

    Exercise 1.4

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,8 +1,17 @@
 const Header = (props) => {
-  console.log(props)
   return (
-        <h1>{props.course}</h1>
-  )
+    <h1>{props.course}</h1>
+  );
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map(part => (
+        <Part part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
 }
 
 const Part = (props) => {
@@ -13,47 +22,30 @@ const Part = (props) => {
   );
 }
 
-const Content = (props) => {
-  console.log(props)
-  return (
-      <div>
-        <Part part={props.part1.name} exercises={props.part1.exercises} />
-        <Part part={props.part2.name} exercises={props.part2.exercises} />
-        <Part part={props.part3.name} exercises={props.part3.exercises} />
-      </div>
-  )
-}
-
 const Total = (props) => {
-  console.log(props);
-  const totalExercises = props.part1.exercises + props.part2.exercises + props.part3.exercises;
+  const totalExercises = props.parts.reduce((total, part) => total + part.exercises, 0);
+
   return (
     <p>Number of exercises {totalExercises}</p>
   );
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const course = 'Half Stack application development';
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
-  )
+  );
 }
 
-export default App
+
+export default App;
