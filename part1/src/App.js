fdf81412b81f1a950380cfefe5015e796e4a1cd0
hangwl <hangwei54e@hotmail.com>
2023-08-19 19:33:12 +0800

    Exercise 1.9

--- part1/src/App.js
+++ part1/src/App.js
@@ -6,10 +6,7 @@ const Button = (props) => (
   </button>
 )
 
-const Statistics = (props) => (
-  <div>
-    {props.label} {props.value}
-  </div>)
+const Statistics = (props) => (<div>{props.label} {props.value}</div>)
 
 const App = () => {
   // save clicks of each button to its own state
@@ -21,6 +18,8 @@ const App = () => {
   const average = (good-bad)/all
   const positive = 100*good/all
 
+  const hasFeedback = all > 0
+
   return (
     <div>
       <h2>give feedback</h2>
@@ -28,12 +27,19 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral+1)} text="Neutral"></Button>
       <Button handleClick={() => setBad(bad+1)} text="Bad"></Button>
       <h2>statistics</h2>
-      <Statistics label="good" value={good}></Statistics>
-      <Statistics label="neutral" value={neutral}></Statistics>
-      <Statistics label="bad" value={bad}></Statistics>
-      <Statistics label="all" value={all}></Statistics>
-      <Statistics label="average" value={average}></Statistics>
-      <Statistics label="positive" value={positive + '%'}></Statistics>
+      {hasFeedback ?(
+        <>
+          <Statistics label="good" value={good}></Statistics>
+          <Statistics label="neutral" value={neutral}></Statistics>
+          <Statistics label="bad" value={bad}></Statistics>
+          <Statistics label="all" value={all}></Statistics>
+          <Statistics label="average" value={average}></Statistics>
+          <Statistics label="positive" value={positive + '%'}></Statistics>
+        </>
+      ) : (
+        <p>No feedback given</p>
+      )}
+
     </div>
   )
 }
