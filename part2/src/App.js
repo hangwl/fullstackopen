6f6d7a7f3654678de99c947135d345e894473651
hangwl <hangwei54e@hotmail.com>
2023-08-23 18:34:17 +0800

    Exercise 2.1

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,18 +1,53 @@
-import Note from './components/Note'
 
+const Course = (props) => {
+  const Header = ({ name }) => {
+    return <h1>{name}</h1>
+  }
 
-const App = ({ notes }) => {
-  console.log(notes)
+  const Content = ({ parts }) => {
+    return (
+      <div>
+      {parts.map(part => (
+        <p key={part.id}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+      </div>
+    )
+  }
+
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(notes => 
-          <Note key={notes.id} note={notes} />
-        )}
-      </ul>
+      <Header name={props.course.name}/>
+      <Content parts={props.course.parts}/>
     </div>
   )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
