09c8d1c53f42f226a785db11c0c507ae8d623ee1
hangwl <hangwei54e@hotmail.com>
2023-08-23 17:57:29 +0800

    part2

---
+++ part2/src/App.js
@@ -0,0 +1,18 @@
+import Note from './components/Note'
+
+
+const App = ({ notes }) => {
+  console.log(notes)
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(notes => 
+          <Note key={notes.id} note={notes} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
